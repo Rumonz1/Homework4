e11069d06417a4204cecaa5f17ad354069d8c149
Rumonz <lone_assassin@mail.ru>
2022-10-27 00:31:12 +0300

    переписал программы с помощью конструкции else

--- src/Main.java
+++ src/Main.java
@@ -4,40 +4,32 @@ public class Main {
         int userAge = 17;
         if (userAge >=18) {
             System.out.println("Поздравляем! Вы уже совершеннолетний!");
-        }
-        if (userAge<18) {
+        }else {
             System.out.println("Вы ещё не достигли совершеннолетнего возраста, пожалуйста подождите немного!");
         }
         //Задание 2
         int childAge = 17;
-        if (childAge >= 7 && childAge < 18){
-            System.out.println("Ребёнок уже ходит в школу!");}
-        if (childAge >= 18 && childAge <24){
-            System.out.println("Человек уже закончил школу и может отправляться в университет!");}
         if (childAge >= 24){
-            System.out.println("Человек окончил университет и ему пора искать первую работу!");}
-        if (childAge < 7){
-            System.out.println("Ребёнок ещё не ходит в школу!");
+            System.out.println("Человек окончил университет и ему пора искать первую работу!");
+        } else if (childAge>=18) {
+            System.out.println("Человек закончил школу и может отправляться в университет!");
+        } else if (childAge>=7) {
+            System.out.println("Ребёнок ходит в школу!");
+        } else if (childAge<7 && childAge>0) {
+            System.out.println("Ребёнок ещё не вырос, чтобы пойти в школу!");
         }
         //Задание 3
         int trainCapacity = 102;
         int seat = 60;
-        int standPlace = 42;
+        int standPlace =42;
         if(seat < 60 && standPlace < 42){
-            System.out.println("В вагоне остались стоячие и сидячие места!");}
-        if(seat == 60 && standPlace == 42){
-            System.out.println("В вагоне не осталось мест");
-        }
-        if(seat < 60 && standPlace == 42){
+            System.out.println("В вагоне остались стоячие и сидячие места!");
+        } else if (seat < 60 && standPlace == 42) {
             System.out.println("В вагоне остались только сидячие места!");
-        }
-        if(seat==60&&standPlace<42){
+        } else if (seat==60&&standPlace<42) {
             System.out.println("В вагоне остались только стоячие места!");
-        }
-        if(seat>60&&standPlace>60){
-            System.out.println("Невозможно разместить больше людей в вагоне");
-        }
-
-
+        } else if (seat == 60 && standPlace == 42) {
+            System.out.println("В вагоне не осталось мест");
+        }else {System.out.println("Невозможно разместить больше людей в вагоне");}
     }
 }
