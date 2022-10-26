d2a4df175b40c0c69a4d4bc174c715515144b689
Rumonz <lone_assassin@mail.ru>
2022-10-26 22:45:55 +0300

    Выполнил задание 1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задание 1
+        int userAge = 17;
+        if (userAge >=18) {
+            System.out.println("Поздравляем! Вы уже совершеннолетний!");
+        }
+        if (userAge<18) {
+            System.out.println("Вы ещё не достигли совершеннолетнего возраста, пожалуйста подождите немного!");
+        }
+    }
+    //Задание 2
+
+}
